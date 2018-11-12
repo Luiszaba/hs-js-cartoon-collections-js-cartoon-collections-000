31d9e830a3b2fa1d8f45a9b619ce4e9b79bcf681
Luis Zabala <luis.zabala076@gmail.com>
2018-11-12 19:40:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+ var dwarvesNum = [];
+  
+  for (let i=0; i < dwarves.length; i++) {
+  dwarvesNum.push([i+1] + '. ' + dwarves[i] + ' ') }
+  
+  return dwarvesNum.join('');
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var Elements = [];
+  
+  for(let i=0; i < planeteerCalls.length; i++) {
+    Elements.push(planeteerCalls[i].toUpperCase() + '!');
+  }
+  return Elements;
 }
 
 function longPlaneteerCalls(words) {
+  if (words.length < 4) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function findTheCheese (foods) {
+var milkProtiens = ["gouda", "camembert", "cheddar"];
+var ingredients = ["garlic", "rosemary", "bread"];
+for (let i = 0; i < foods.length; i++) {
+  if (foods[i] == "gouda" || foods[i] == "cheddar" || foods[i] == "camembert")
+  return "cheddar";}
+{
+return "no cheese!";
+}
 }
